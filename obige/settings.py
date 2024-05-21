e0da2afe5bf9bd7e1b3384ca9ea2b9942bfc81f5
oleg14422 <166949785+oleg14422@users.noreply.github.com>
2024-05-21 20:37:52 +0300

    Update settings.py

--- obige/settings.py
+++ obige/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5g+xn=7mh!b7h9iw!suwxx4u#zr1yjg6g90nbu@khug=pi=t#u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
